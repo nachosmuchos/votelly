90babfc5bd90bda5fb3efa97e878340181419bf2
nachosmuchos <kattis@wippies.com>
2018-08-23 21:01:51 +0300

    add character photo

--- characters/models.py
+++ characters/models.py
@@ -6,6 +6,7 @@ class Character(models.Model):
     name = models.CharField(max_length=200)
     number_of_votes = models.IntegerField(default=0)
     related_program = models.ForeignKey(Program, on_delete=models.CASCADE, default=1)
+    character_photo = models.ImageField(upload_to='media/')
 
     def __str__(self):
         return self.name[:100]
