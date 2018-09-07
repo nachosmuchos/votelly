28e3dc33d9700e642e26b110b1caddbe24d0eced
nachosmuchos <kattis@wippies.com>
2018-09-07 18:58:14 +0300

    removed static paths from common settings

--- votelly/settings/common.py
+++ votelly/settings/common.py
@@ -119,11 +119,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
-MEDIA_URL = '/pics/'
-MEDIA_ROOT = BASE_DIR
