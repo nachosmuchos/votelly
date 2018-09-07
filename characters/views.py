ea3bc123513bdc06b7505858bac71572feb98622
nachosmuchos <kattis@wippies.com>
2018-09-07 16:06:05 +0300

    progress bar width to available space

--- characters/views.py
+++ characters/views.py
@@ -9,8 +9,9 @@ def character_comments(request, fk):
     characters = Character.objects.filter(related_program = character.related_program).order_by('-number_of_votes')
     program = character.related_program
     total_votes = total_character_votes(characters)
+    max_votes = max_character_votes(characters)
     template_path = 'comments/comment.html'
-    context_dictionary = {'comments':comments, 'program':program, 'character':character, 'characters':characters, 'total_votes':total_votes}
+    context_dictionary = {'comments':comments, 'program':program, 'character':character, 'characters':characters, 'total_votes':total_votes, 'max_votes':max_votes}
     return render(request, template_path, context_dictionary)
 
 def vote_character(request, fk):
@@ -26,9 +27,10 @@ def vote_character(request, fk):
          character.number_of_votes += 1
          character.save()
          total_votes = total_character_votes(characters)
+         max_votes = max_character_votes(characters)
          comments = Comment.objects.filter(related_character_id = fk).order_by('-comment_time')[0:20]
          template_path = 'comments/comment.html'
-         context_dictionary = {'comments':comments, 'program':program, 'character':character, 'characters':characters, 'total_votes':total_votes}
+         context_dictionary = {'comments':comments, 'program':program, 'character':character, 'characters':characters, 'total_votes':total_votes, 'max_votes':max_votes}
          return render(request, template_path, context_dictionary)
 
 def total_character_votes(chars):
@@ -36,3 +38,10 @@ def total_character_votes(chars):
     for character in chars:
         total = total + character.number_of_votes
     return(total)
+
+def max_character_votes(chars):
+    maximum = 0
+    for character in chars:
+        if character.number_of_votes > maximum:
+            maximum = character.number_of_votes
+    return(maximum)
--- programs/views.py
+++ programs/views.py
@@ -11,14 +11,21 @@ def program_characters(request, fk):
     characters = Character.objects.filter(related_program_id = fk).order_by('-number_of_votes')
     program = Program.objects.filter(id = fk)
     total_votes = total_character_votes(characters)
-#    import pdb; pdb.set_trace()
-    return render(request, 'characters/vote.html', {'characters':characters, 'program':program, 'total_votes':total_votes})
+    max_votes = max_character_votes(characters)
+    return render(request, 'characters/vote.html', {'characters':characters, 'program':program, 'total_votes':total_votes, 'max_votes':max_votes})
 
 def total_character_votes(chars):
     total = 0
     for character in chars:
         total = total + character.number_of_votes
     return(total)
+
+def max_character_votes(chars):
+    maximum = 0
+    for character in chars:
+        if character.number_of_votes > maximum:
+            maximum = character.number_of_votes
+    return(maximum)
 
 def disclaimer(request):
     return render(request, 'programs/disclaimer.html')
